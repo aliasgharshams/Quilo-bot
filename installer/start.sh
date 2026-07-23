bd21cfdffe0533f834fe4e7f1fb80c7e45abdad5
Quilo <admin@netlinr.shop>
2026-07-23 20:55:34 +0000

    Add one line installer

---
+++ installer/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+
+echo "Downloading Quilo..."
+
+cd /tmp
+
+rm -rf Quilo-bot
+
+git clone https://github.com/aliasgharshams/Quilo-bot.git
+
+cd Quilo-bot
+
+chmod +x installer/install.sh
+
+bash installer/install.sh
