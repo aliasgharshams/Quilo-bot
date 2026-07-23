381f03393fecb5d60178008f4eed88b0a0964c28
Quilo <admin@netlinr.shop>
2026-07-23 13:39:44 +0000

    Add safe config example

---
+++ app/config/config.example.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    "telegram" => [
+        "token" => "YOUR_BOT_TOKEN",
+        "admin_id" => "YOUR_ADMIN_ID"
+    ],
+
+    "bot" => [
+        "username" => "BOT_USERNAME"
+    ],
+
+    "db" => [
+        "host" => "localhost",
+        "name" => "quilo",
+        "user" => "quilo_user",
+        "pass" => "YOUR_PASSWORD"
+    ]
+
+];
